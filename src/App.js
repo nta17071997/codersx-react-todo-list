7d5a4ef21a9a43a07b436392063e733cb8a78c92
An <nguyenthanhan.binhminh@gmail.com>
2020-06-10 15:56:18 +0700

    Add commit onClickedItem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <li>Go to market</li>
-        <li>Buy food</li>
-        <li>Make dinner</li>
-      </ul>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: [
+        { title: "Go to market", isComplete: true },
+        { title: "Buy food", isComplete: true },
+        { title: "Make dinner", isComplete: false },
+      ],
+    };
+  }
+  onItemClicked = (item) => {
+    const { todoItems } = this.state;
+    const isComplete = item.isComplete;
+    const index = todoItems.indexOf(item);
+    this.setState({
+      todoItems: [
+        ...todoItems.slice(0, index), //tu vi tri 0 den index
+        {
+          ...item,
+          isComplete: !isComplete,
+        },
+        ...todoItems.slice(index + 1), //k laay index: tu index => heet
+      ],
+    });
+  };
+  render() {
+    const { todoItems } = this.state;
+    return (
+      <div className="App">
+        <h1>TODO - LIST</h1>
+        {todoItems.length > 0 &&
+          todoItems.map((item, index) => {
+            return (
+              <TodoItem
+                key={index}
+                item={item}
+                onClick={() => this.onItemClicked(item)}
+              />
+            );
+          })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import correct from "./images/correct.svg";
+import success from "./images/success.svg";
+
+class TodoItem extends Component {
+  render() {
+    const { item, onClick } = this.props;
+    return (
+      <div className="item">
+        <img
+          onClick={onClick}
+          alt=""
+          src={item.isComplete === true ? correct : success}
+        />
+        <p className={item.isComplete === true ? "isComplete" : ""}>
+          {item.title}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
